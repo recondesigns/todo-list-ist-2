0ea2bbbb5dedbe3c7480d352c02049373e921f65
Michael Stedman <michaelstedman81@gmail.com>
2020-08-30 22:23:14 -0500

    Commit
    Commit

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const app = express()
 const port = process.env.PORT || 8000
 
 app.use(cors())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "client", "build")))
 
@@ -19,6 +25,7 @@ const connection = mongoose.connection
 connection.once('open', () => console.log(`Connected to dB.`))
 
 const usersRouter = require('./routes/users')
+const { readSync } = require('fs')
 app.use('/users', usersRouter)
 
 app.get("*", (req, res) => {
